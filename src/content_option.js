b550e310362654702c8a2ec0498e275c297da1df
Amr Helmy <107793670+Amr-helmy0@users.noreply.github.com>
2024-09-25 15:28:02 +0300

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,129 @@
+const logotext = "AM";
+const meta = {
+  title: "Amr Helmy",
+  description: "I’m Amr Helmy  _UI-UX Designer,currently working in Egypt",
+};
+
+const introdata = {
+  title: "I’m Amr helmy",
+  animated: {
+    first: "ICreate UI-UX Design",
+    second: "Create Wireframing",
+    third: "Create Prototype",
+  },
+  description:
+    "I am a talented, ambitious and hardworking person, with extensive UI-UX Design skills and experience. Furthermore, I am adept at handling",
+  your_img_url:
+    "https://i.postimg.cc/vHWGgwv1/Whats-App-Image-2024-09-25-at-14-17-17-37b195b1.jpg",
+};
+
+const dataabout = {
+  title: "About MySelf",
+  aboutme:
+    "I am UI/UX designer passionate about creating inspiring designs for users. My goal is to continuously learn, share useful tricks,and strive for excellence in my work, knowing that mistakes are part of the learning process.",
+};
+const worktimeline = [
+  {
+    jobtitle: "IT-Gates",
+    where: "Egypt",
+    date: "2023 - Till Now",
+  },
+  {
+    jobtitle: "DIREXIONNL",
+    where: "Egypt",
+    date: "2023",
+  },
+  {
+    jobtitle: "Freelance (UI-UX-Web Design)",
+    where: "Egypt",
+    date: "2023",
+  },
+];
+
+const skills = [
+  {
+    name: "UI-UX Design",
+    value: 90,
+  },
+  {
+    name: "Testing",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "UI-UX Design",
+    description:
+      " curated collection of intuitive, user-centered designs that blend functionality and aesthetics. My work focuses on creating seamless digital experiences with a strong emphasis on usability and visual appeal.",
+  },
+  {
+    title: "Testing",
+    description:
+      "Ensuring software quality through meticulous testing processes, focusing on functionality and user experience. Skilled in identifying issues to deliver reliable and bug-free applications.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://i.ibb.co/hYGCTyN/delvery-app.png",
+    description:
+      "The food delivery app showcases a sleek, modern design focused on user-friendly navigation. It features clear and simple interfaces, offering easy access to restaurant selections, item customization, and shopping cart management.",
+    link: "https://www.behance.net/gallery/208446195/Food-Delivery-App",
+  },
+  {
+    img: "https://i.postimg.cc/0j9dMpdV/BEE.png",
+    description:
+      "The app design showcases a clean and modern UI for an Arabic learning platform, focusing on vocabulary through interactive games, tasks, and worksheets.The color scheme is warm and inviting with yellow and orange tones, while the interface elements like buttons and icons are simple, user-friendly, and optimized for a smooth learning experience.",
+    link: "https://www.behance.net/gallery/208552721/arabee",
+  },
+  {
+    img: "https://i.postimg.cc/D0c6BCwV/ricpe.png",
+    description:
+      "The recipe app design emphasizes clean aesthetics, intuitive navigation, and a structured layout. It features sections for personalized recipe suggestions, detailed ingredient lists, and step-by-step cooking instructions, making it easy for users to explore and follow recipes.",
+    link: "https://www.behance.net/gallery/208445997/Recipe",
+  },
+  {
+    img: "https://i.postimg.cc/qvJSV3PX/market.png",
+    description:
+      "The interior design e-commerce app presents a minimalistic and elegant interface, emphasizing comfort and aesthetics. It features a clean layout with high-quality imagery, simple navigation, and a prominent product display, enhancing the visual appeal of the furniture and décor items.",
+    link: "https://www.behance.net/gallery/208446445/Interior",
+  },
+  {
+    img: "https://i.postimg.cc/CLmm3LbW/PULPO-1.png",
+    description:
+      "The app design for Pulpo CRM features a sleek, minimalistic interface with a purple and white color scheme, providing an intuitive and secure user experience. Key features include streamlined navigation, data management, and user authentication, optimized for efficiency and clarity.",
+    link: "https://www.behance.net/gallery/208444823/pulpo-crm",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "amrhe4030@gmail.com",
+  YOUR_FONE: "(+20)01009152403",
+  description:
+    "I am a talented, ambitious and hardworking person, with extensive web development skills and experience.Furthermore, I am adept at handling multiple tasks on a. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/Amr-helmy0",
+  facebook: "https://www.facebook.com/mero.helmy.1612/",
+  linkedin: "https://www.facebook.com/mero.helmy.1612/",
+  twitter: "https://twitter.com/amrhelm0",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
